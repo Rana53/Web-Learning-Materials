44697b550b4eeabce555667da2e0582e091dee46
Rana53 <ranajahid.cseru@gmail.com>
2019-11-23 20:10:05 +0600

    create a nav bar using stateless component and pass data by props

--- JavaScript/ReactJS/react-basics/src/App.js
+++ JavaScript/ReactJS/react-basics/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="container">
       <div className="row">
         <div className="col-xs-10 col-xs-offset-1">
-          <Header/>
-        </div>
+          <Header homeLink="Home"/>
+        </div> 
       </div>
     </div>
   );

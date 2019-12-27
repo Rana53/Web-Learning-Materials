0ceb40c9c19e2d7b45b0dace8ef1ed2c5a5b402d
Rana53 <ranajahid.cseru@gmail.com>
2019-12-27 21:29:18 +0600

    nav bar aded for chacking container

--- JavaScript/ReactJS/react-bootstrap/src/App.js
+++ JavaScript/ReactJS/react-bootstrap/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   
   render(){
     return(
-      <Fragment>
+      <Fragment> 
       <div className="container">
         <div className="row">
            <div className="col-md-6">
